b52727c4b6c230d3ef60e3a015819a6c9e3c269e
majirus <jmfansi@gmail.com>
2013-04-17 03:41:33 +0200

    initial commit of solr setup project

---
+++ solr-custom-project-library/src/main/java/fr/valtech/solr/library/CustomSimilarity.java
@@ -0,0 +1,13 @@
+package fr.valtech.solr.library;
+
+import org.apache.lucene.search.similarities.DefaultSimilarity;
+
+/**
+ * Implementer ici une classe de similarité particulière
+ * @author mfansi
+ *
+ */
+public class CustomSimilarity extends DefaultSimilarity {
+
+  
+}
